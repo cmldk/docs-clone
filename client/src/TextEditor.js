29c60e34d9298a9c99b0cc8621a265aa4951de4e
cmldk <cemal.dak@gmail.com>
2022-07-12 23:35:25 +0300

    change toolbar options

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -6,23 +6,15 @@ import { useParams } from 'react-router-dom';
 
 const SAVE_INTERVAL_MS = 2000;
 const TOOLBAR_OPTIONS = [
-  ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-  ['blockquote', 'code-block'],
-
-  [{ header: 1 }, { header: 2 }], // custom button values
-  [{ list: 'ordered' }, { list: 'bullet' }],
-  [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-  [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-  [{ direction: 'rtl' }], // text direction
-
-  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
   [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
   [{ align: [] }],
-
-  ['clean'], // remove formatting button
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
 ];
 
 export default function TextEditor() {
